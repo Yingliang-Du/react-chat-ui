69d554ae07f730455f834da582bd1169704b4033
Yingliang Du <yingliang.du1@ibm.com>
2020-06-25 16:13:16 -0400

    Wrap app with Redux so the global store can be accessed

--- src/index.js
+++ src/index.js
@@ -3,15 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
 
 import { createStore } from 'redux';
 import allReducers from './redux/reducers'
 
-const store = createStore(allReducers);
+const store = createStore(
+    allReducers, 
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
