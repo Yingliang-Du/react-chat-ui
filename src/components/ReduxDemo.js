53ca9e6df99810652b0ad436ec4e5b474ee3f50b
Yingliang Du <yingliang.du1@ibm.com>
2020-06-26 06:47:34 -0400

    hook up the action

--- src/components/ReduxDemo.js
+++ src/components/ReduxDemo.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement } from '../redux/actions'
 
 const ReduxDemo = () => {
     const counter = useSelector(state => state.counter);
     const flag = useSelector(state => state.flag);
 
+    const dispatch = useDispatch();
+
     return (
         <div>
             <h1>Counter: {counter}</h1>
+            <button onClick={() => dispatch(increment())}>+</button>
+            <button onClick={() => dispatch(decrement())}>-</button>
 
             {flag ? <h3>Flag was ON!</h3> : <h3>Flag was OFF!</h3>}
         </div>
